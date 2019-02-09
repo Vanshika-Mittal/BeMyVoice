a7a2977b20734828bd3d3ae4a492b939363e413d
Vanshika-Mittal <Vanshika-Mittal@users.noreply.github.com>
2019-02-09 14:30:31 +0530

    Logo added More minute changes done

--- app/src/main/java/com/example/android/voiceassistantcommands/CatalogActivity.java
+++ app/src/main/java/com/example/android/voiceassistantcommands/CatalogActivity.java
@@ -46,6 +46,8 @@ public class CatalogActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
+        this.setTitle(getResources().getString(R.string.catalog_activity_label));
+
         // Setup FAB to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -162,30 +164,6 @@ public class CatalogActivity extends AppCompatActivity
 
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_catalog.xml file.
-        // This adds menu items to the app bar.
-        getMenuInflater().inflate(R.menu.menu_catalog, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // User clicked on a menu option in the app bar overflow menu
-        switch (item.getItemId()) {
-            // Respond to a click on the "Insert dummy data" menu option
-            case R.id.action_insert_dummy_data:
-                return true;
-            // Respond to a click on the "Delete all entries" menu option
-            case R.id.action_delete_all_entries:
-                // Do nothing for now
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 
